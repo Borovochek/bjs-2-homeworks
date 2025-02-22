ed78c09f9f7ee1394dfa3741ee866ccc90aef3a6
Ant <jabbahatit@gmail.com>
2025-02-22 18:58:51 +0300

    1 редактирование 1 задания

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,22 +3,14 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let discriminant = b ** 2 - (4 * a * c);
-
-  if (discriminant < 0) {
-    return arr;
-  }
-
   if (discriminant === 0) {
     let root = -b / (2 * a);
     arr.push(root);
-  }
-
-  else {
+  } else if (discriminant > 0) {
     let root1 = (- b + Math.sqrt(discriminant) ) / (2 * a);
     let root2 = (- b - Math.sqrt(discriminant) ) / (2 * a);
     arr.push(root1, root2);
   }
-
   return arr;
 }
 
@@ -26,12 +18,10 @@ function solveEquation(a, b, c) {
 
 // Задание №2
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let P = (percent / 100) / 12;
-  let S = amount - contribution;
-  let n = countMonths;
-  let monthlyPayment = S * (P + (P / (((1 + P) ** n) - 1)));
-  let result = monthlyPayment * n;
-
+  let monthPercent = (percent / 100) / 12;
+  let creditSum = amount - contribution;
+  let monthlyPayment = creditSum * (monthPercent + (monthPercent / (((1 + monthPercent) ** countMonths) - 1)));
+  let result = monthlyPayment * countMonths;
   return Number(result.toFixed(2));
 }
 
